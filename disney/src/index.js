1adabd0cb6de23c0138fbf8a0d75e447245999d7
Sadrah Alsabagh <sedrasabbagh33@gmail.com>
2023-03-06 20:58:13 +0300

    Add sign in functionality with redux

--- disney/src/index.js
+++ disney/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 import './index.css';
 import App from './App';
@@ -8,9 +10,11 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
