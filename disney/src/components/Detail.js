f89a9d5a07782428739c324cec1a036b9a8af09d
Sadrah Alsabagh <sedrasabbagh33@gmail.com>
2023-03-15 19:49:12 +0300

    Add image title

--- disney/src/components/Detail.js
+++ disney/src/components/Detail.js
@@ -7,6 +7,10 @@ const Detail = (props) => {
             <Background>
         <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/49B92C046117E89BC9243A68EE277A3B30D551D4599F23C10BF0B8C1E90AEFB6/scale?width=1440&aspectRatio=1.78&format=jpeg"/>
       </Background>
+      <ImgTitle>
+      <img src='https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/5C647DF3FFBFA343CFEA84AC715148F25F9E86F398B408010CC403E7654FB908/scale?width=1440&aspectRatio=1.78'/>
+
+      </ImgTitle>
         </Container>
     )
 };
@@ -35,6 +39,24 @@ const Background = styled.div`
     @media (max-width: 768px) {
       width: initial;
     }
+  }
+`;
+
+const ImgTitle = styled.div`
+  align-items: flex-end;
+  display: flex;
+  -webkit-box-pack: start;
+  justify-content: flex-start;
+  margin: 0px auto;
+  height: 30vw;
+  min-height: 170px;
+  padding-bottom: 24px;
+  width: 100%;
+
+  img {
+    max-width: 600px;
+    min-width: 200px;
+    width: 35vw;
   }
 `;
 
